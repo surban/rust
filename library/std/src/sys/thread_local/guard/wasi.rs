4c2e45aa94479f2c7f5b34847a9e9075d2fe2d0c
Sebastian Urban <surban@surban.net>
2024-11-25 10:55:59 +0100

    Run TLS destructors for main thread.

--- library/std/src/sys/thread_local/guard/wasi.rs
+++ library/std/src/sys/thread_local/guard/wasi.rs
@@ -1,13 +1,14 @@
 //! wasm32-wasip1 has pthreads support.
 
-use core::{mem, ptr};
-
 use crate::cell::Cell;
-use crate::ffi;
+use crate::sync::atomic::{AtomicBool, Ordering};
 use crate::sys::thread_local::destructors;
+use crate::{ffi, mem, ptr};
 
 // Add a few symbols not in upstream `libc` just yet.
 mod libc {
+    pub use libc::*;
+
     use crate::ffi;
 
     #[allow(non_camel_case_types)]
@@ -24,22 +25,44 @@ mod libc {
 }
 
 pub fn enable() {
+    enable_main();
+    enable_thread();
+}
+
+fn enable_main() {
+    static REGISTERED: AtomicBool = AtomicBool::new(false);
+
+    if !REGISTERED.swap(true, Ordering::AcqRel) {
+        unsafe {
+            assert_eq!(libc::atexit(run_main_dtors), 0);
+        }
+    }
+
+    extern "C" fn run_main_dtors() {
+        unsafe {
+            destructors::run();
+            crate::rt::thread_cleanup();
+        }
+    }
+}
+
+fn enable_thread() {
     #[thread_local]
     static REGISTERED: Cell<bool> = Cell::new(false);
 
     if !REGISTERED.replace(true) {
         unsafe {
             let mut key: libc::pthread_key_t = mem::zeroed();
-            assert_eq!(libc::pthread_key_create(&mut key, run_dtors), 0);
+            assert_eq!(libc::pthread_key_create(&mut key, run_thread_dtors), 0);
 
             // We must set the value to a non-NULL pointer value so that
             // the destructor is run on thread exit. The pointer is only
             // passed to run_dtors and never dereferenced.
-            assert_eq!(libc::pthread_setspecific(key, ptr::with_exposed_provenance(8)), 0);
+            assert_eq!(libc::pthread_setspecific(key, ptr::without_provenance(1)), 0);
         }
     }
 
-    extern "C" fn run_dtors(_: *mut ffi::c_void) {
+    extern "C" fn run_thread_dtors(_: *mut ffi::c_void) {
         unsafe {
             destructors::run();
             crate::rt::thread_cleanup();
